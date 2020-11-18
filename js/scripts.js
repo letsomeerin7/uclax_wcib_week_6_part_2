9990ebe94813696b0e8c5364178fa3b3f799753a
letsomeerin7 <erinjamison@erins-mbp.home>
2020-11-17 16:39:08 -0800

    fix it all

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,6 @@
 console.log('Hello from js/scripts.js!');
 
 function changeImage(imgName)
- {
-    image = document.getElementById('kinEuphorics');
+ {  image = document.getElementById('kinEuphorics');
     image.src = imgName;
  }
